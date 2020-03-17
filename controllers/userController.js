07e4f55e91b80b99fbf2e1bd10ac658a6a34c9cd
Hans Vos <hans.vos@codeimmersives.com>
2020-03-17 11:34:36 -0400

    added userController.js with register

---
+++ controllers/userController.js
@@ -0,0 +1,37 @@
+const User = ('../routes/users/User');
+const bcrypt = ('bcryptjs');
+const passport = ('passport');
+require('../lib/passport');
+
+module.exports = {
+    register: (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array() })
+        }
+        const { name, email, password } = req.body;
+        User.findOne({email: email}).then((user) => {
+          if (user) {
+            //return req.flash('errors', 'User Already exists')
+            return res.send('User Exists')
+          } else {
+              const newUser = new User();
+        
+          // const salt = bcrypt.genSaltSync(10);
+          // const hash = bcrypt.hashSync(params.password, salt);
+        
+          newUser.name = name;
+          newUser.email = email;
+          newUser.password = password;
+        
+          newUser.save().then((user) => {
+                if(user) {
+                  res.status(200).json({message: 'success', user})
+                }
+              }).catch(err=> {
+                return next(err);
+              })
+                } 
+              })
+    }
+};
--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -5,43 +5,15 @@ const passport = require('passport');
 const {validationResult} = require('express-validator');
 const userValidation = require('../utils/userValidation');
 const User = require('./models/User')
+const userController = require('../../controllers/userController')
 require('../../lib/passport')
+
 
 router.get('/register', (req, res) => {
     res.render('auth/register')
 })
 
-router.post('/register', (req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(422).json({errors: errors.array() })
-    }
-    const { name, email, password } = req.body;
-    User.findOne({email: email}).then((user) => {
-      if (user) {
-        //return req.flash('errors', 'User Already exists')
-        return res.send('User Exists')
-      } else {
-          const newUser = new User();
-    
-      // const salt = bcrypt.genSaltSync(10);
-      // const hash = bcrypt.hashSync(params.password, salt);
-    
-      newUser.name = name;
-      newUser.email = email;
-      newUser.password = password;
-    
-      newUser.save().then((user) => {
-            if(user) {
-              res.status(200).json({message: 'success', user})
-            }
-          }).catch(err=> {
-            return next(err);
-          })
-            } 
-          })
-}, userValidation);
-
+router.post('/register', userController.register, userValidation);
 
 
 router.get('/login', (req, res) => {
