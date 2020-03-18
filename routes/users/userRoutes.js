db57df462ded1dc09fc474faeb2ca4118485cf64
Hans Vos <hans.vos@codeimmersives.com>
2020-03-18 14:52:48 -0400

    working with api

--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -12,6 +12,13 @@ router.get('/register', (req, res) => {
     res.render('auth/register')
 })
 
+router.get('/nba', async (req, res) => {
+  const api_url = 'https://api-nba-v1.p.rapidapi.com/3e70e4bac1msh7c64682d89b9128p147368jsn5176252e429f';
+  const fetch_response = await fetch(api_url);
+  const json = await fetch_response.json();
+  console.log(json);
+})
+
 router.post('/register', userController.register, userValidation);
 
 router.get('/login', (req, res) => {
