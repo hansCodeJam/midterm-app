758b151104d8f32447430f5b1ea2ae64180ff16f
Hans Vos <hans.vos@codeimmersives.com>
2020-03-19 15:07:49 -0400

    deleted apiRoutes.js, added bootsnipped to both login and register

--- routes/api/apiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const basketballController = require('../../controllers/nbaController');
-
-router.route('/updates').get(basketballController);
-
-module.exports = router;
