29795560569b8c1a84af6cc465179c911490a66c
Hans Vos <hans.vos@codeimmersives.com>
2020-03-19 14:30:34 -0400

    api working, added bootsnipped template to login, partials not working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const axios = require("axios");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
+axios({
+    "method":"GET",
+    "url":"https://api-nba-v1.p.rapidapi.com/players/lastName/bryant",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"api-nba-v1.p.rapidapi.com",
+    "x-rapidapi-key":"3e70e4bac1msh7c64682d89b9128p147368jsn5176252e429f" 
+    }
+    })
+    .then((response)=>{
+      console.log(response.data.api.players);
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
   res.render('index', { title: 'Express' });
 });
 
