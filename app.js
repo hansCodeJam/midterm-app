40f3f97c83c8ba87cce6d39f8d1f8f4bde3f9598
Hans Vos <hans.vos@codeimmersives.com>
2020-03-21 17:31:56 -0400

    image not working

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const methodOverride = require('method-override');
 const cors = require('cors');
 
 const indexRouter = require('./routes/index');
-const basketballRouter = require('./routes/api/apiRoutes')
 const usersRouter = require('./routes/users/userRoutes');
 
 const app = express();
@@ -57,7 +56,6 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use('/', indexRouter);
-app.use('/api/v1', basketballRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
