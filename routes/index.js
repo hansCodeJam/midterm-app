fa76d1ca68017cbc406113b925e53853a4a19cc1
Hans Vos <hans.vos@codeimmersives.com>
2020-03-23 12:35:20 -0400

    using different third party api

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,45 @@
-var express = require('express');
-var router = express.Router();
-const axios = require("axios");
+const express = require('express');
+const router = express.Router();
+// const axios = require("axios");
+const fetch = require('node-fetch');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+// router.get('/', function(req, res, next) {
   
-axios({
-    "method":"GET",
-    "url":"https://api-nba-v1.p.rapidapi.com/players/lastName/bryant",
-    "headers":{
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"api-nba-v1.p.rapidapi.com",
-    "x-rapidapi-key":"3e70e4bac1msh7c64682d89b9128p147368jsn5176252e429f" 
-    }
-    })
-    .then((response)=>{
-      console.log(response.data.api.players);
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
-  res.render('index', { title: 'Express' });
-});
+// axios({
+//     "method":"GET",
+//     "url":"https://api-nba-v1.p.rapidapi.com/players/lastName/bryant",
+//     "headers":{
+//     "content-type":"application/octet-stream",
+//     "x-rapidapi-host":"api-nba-v1.p.rapidapi.com",
+//     "x-rapidapi-key":"3e70e4bac1msh7c64682d89b9128p147368jsn5176252e429f" 
+//     }
+//     })
+//     .then((response)=>{
+//       console.log(response.data.api.players);
+//     })
+//     .catch((error)=>{
+//       console.log(error)
+//     })
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+// module.exports = router;
+
+router.get('/', function(req, res, next) {
+  let url = "https://nba-players.herokuapp.com/players-stats";
+
+  let settings = { method: "Get" };
+  
+  fetch(url, settings)
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data[0].name)
+      });
+  res.render('index', {title: 'Express'})
+  });
+  
+  module.exports = router;
+
+
